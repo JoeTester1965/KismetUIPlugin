bd68ccdcfd3598a8876e94b83e01fbbca25dc654
joetester1965 at maildotcom <24988751+JoeTester1965@users.noreply.github.com>
2022-06-23 22:35:35 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 pkill -f kismet
 pkill -f KismetUIPlugin.py
 nohup kismet 2>/dev/null &
 sleep 1
-nohup python KismetUIPlugin.py 2>/dev/null &
+sudo -H -u pi nohup python KismetUIPlugin.py 2>/dev/null &
