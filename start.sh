80e3ef65a14f5007ba24b13709a70361e8cc0a70
joetester1965 at maildotcom <24988751+JoeTester1965@users.noreply.github.com>
2023-04-10 13:00:49 +0100

    Update start.sh
    Assume that kismet is installed with setuid so no root required to run

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo pkill -f kismet
+pkill -f kismet
 pkill -f KismetUIPlugin.py
-sudo nohup kismet 2>/dev/null &
+nohup kismet 2>/dev/null &
 sleep 1
 nohup python KismetUIPlugin.py 2>/dev/null &
