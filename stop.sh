57b1ea8bfcc6cb6984c3a0ce90a2b025016c821e
joetester1965 at maildotcom <24988751+JoeTester1965@users.noreply.github.com>
2023-04-11 10:21:15 +0100

    Update stop.sh
    doh!

--- stop.sh
+++ stop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-killall -9 kismet
-sleep 5
-rm *.kismet 2>/dev/null
-rm *.kismet-journal 2>/dev/null
-pkill -f KismetUIPlugin.py
+killall -9 kismet 2>/dev/null 
+sudo rm *.kismet 2>/dev/null 
+sudo rm *.kismet-journal 2>/dev/null
+pkill -f KismetUIPlugin.py 2>/dev/null 
