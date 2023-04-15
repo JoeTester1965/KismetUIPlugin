5973bf5fc7ff0642539630cb8fb0fb0ea09f8573
joetester1965 at maildotcom <24988751+JoeTester1965@users.noreply.github.com>
2023-04-15 08:21:41 +0100

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-killall -9 kismet 2>/dev/null 
+pkill -f kismet 2>/dev/null 
 sudo rm *.kismet 2>/dev/null 
 sudo rm *.kismet-journal 2>/dev/null
 pkill -f KismetUIPlugin.py 2>/dev/null 
