6a5ec702bda84dc35aece3828e87e1104c2a8a7f
joetester1965 at maildotcom <24988751+JoeTester1965@users.noreply.github.com>
2023-04-16 10:19:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ./stop.sh 2>/dev/null
-nohup kismet 2>/dev/null &
-nohup python KismetUIPlugin.py 2>/dev/null &
+nohup /usr/local/bin/kismet &> kismet.log &
+nohup python KismetUIPlugin.py &>KismetUIPlugin.log &
