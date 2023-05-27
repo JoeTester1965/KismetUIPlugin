23b2a6d073817ca6433e94e21773f7e4c02a4548
joetester1965 at maildotcom <24988751+JoeTester1965@users.noreply.github.com>
2023-05-27 14:22:15 +0100

    Update start.sh
    Need to specify python3 for Buster

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 ./stop.sh 2>/dev/null
 nohup /usr/local/bin/kismet &> kismet.log &
 nohup /usr/bin/kismet &> kismet.log &
-nohup python KismetUIPlugin.py &>KismetUIPlugin.log &
+nohup python3 KismetUIPlugin.py &>KismetUIPlugin.log &
