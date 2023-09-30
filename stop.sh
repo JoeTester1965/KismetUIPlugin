49c226182153ef3ae2a97041c873439e3cbe0d5b
joetester1965 at maildotcom <24988751+JoeTester1965@users.noreply.github.com>
2023-09-30 10:35:11 +0100

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-pkill -f kismet &> /dev/null
+pkill -f kismet_cap_linux_wifi > /dev/null
+pkill -f kismet > /dev/null
 pkill -f KismetUIPlugin.py &> /dev/null
 sudo rm *.kismet &> /dev/null
 sudo rm *.kismet-journal &> /dev/null
+sudo rm *.log &> /dev/null
 sudo rm *.csv &> /dev/null
