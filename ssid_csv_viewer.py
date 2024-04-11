549b3341a46098ab382bad68a8eafd38826f03b1
D P <24988751+JoeTester1965@users.noreply.github.com>
2024-04-11 15:50:34 +0100

    tidied

--- ssid_csv_viewer.py
+++ ssid_csv_viewer.py
@@ -21,31 +21,10 @@ ssid_df = pd.read_csv(csvfile).dropna()
 ssid_df['start_time_datetime'] = pd.to_datetime(ssid_df['start_time'])
 ssid_df['end_time_datetime'] = pd.to_datetime(ssid_df['end_time'])
 
-#ssid_df_temp = ssid_df.loc[~((ssid_df['advertising_devices_len'] == 0) & (ssid_df['responding_devices_len'] == 0)),:]
-
-#graph = ggplot(ssid_df_temp) + geom_errorbar(aes(x = 'ssid', ymax = 'start_time_datetime', ymin = 'end_time_datetime' ), size = 2, color = "grey") + \
-#        ylab("date") + theme(axis_text_x=element_text(rotation=90, size=8)) + \
-#        scale_y_datetime(date_breaks = "1 hour", labels = date_format("%d-%m-%Y %H")) + \
-#        theme(axis_text_y=element_text(size=6)) + coord_flip()  + theme(figure_size=(12, 12)) + \
-#        ggtitle("SSIDs advertising or responding to probes")
-
-#print(graph)
-
-#ssid_df_temp = ssid_df.loc[((ssid_df['advertising_devices_len'] == 0) & (ssid_df['responding_devices_len'] == 0)),:]
-
-#graph = ggplot(ssid_df_temp) + geom_errorbar(aes(x = 'ssid', ymax = 'start_time_datetime', ymin = 'end_time_datetime' ), size = 2, color = "grey") + \
-#        ylab("date") + theme(axis_text_x=element_text(rotation=90, size=6)) + \
-#        scale_y_datetime(date_breaks = "1 hour", labels = date_format("
-# %d-%m-%Y %H")) + \
-#        theme(axis_text_y=element_text(size=6)) + theme(figure_size=(12, 12)) + \
-#        ggtitle("Probed SSIDs not advertising or responding")
-
-#print(graph)
-
 ssid_df_temp = ssid_df.loc[((ssid_df['advertising_devices_len'] == 0) & (ssid_df['responding_devices_len'] == 0)),:]
 
 frequency_ssid = ssid_df_temp['ssid'].value_counts()
-frequency_indexes = frequency_ssid[frequency_ssid > times_seen_threshold].index 
+frequency_indexes = frequency_ssid[frequency_ssid >= times_seen_threshold].index 
 ssid_df_temp_filtered = ssid_df_temp[ssid_df_temp['ssid'].isin(frequency_indexes)]
 
 for index,row in ssid_df_temp_filtered.iterrows():
@@ -58,7 +37,7 @@ graph = ggplot(ssid_df_temp_filtered) + geom_errorbar(aes(x = 'ssid', ymax = 'st
         ylab("Hour") + theme(axis_text_x=element_text(rotation=90, size=6)) + \
         scale_y_datetime(date_breaks = "1 hour", labels = date_format("%H")) + \
         theme(axis_text_y=element_text(size=6)) + theme(figure_size=(12, 12)) + \
-      ggtitle(f"Probed SSIDs not advertising or responding, seen more than {times_seen_threshold} times wrapped every 24 hours")
+      ggtitle(f"Probed SSIDs not advertising or responding, seen at least {times_seen_threshold} times and wrapped every 24 hours")
 
 print(graph)
 
