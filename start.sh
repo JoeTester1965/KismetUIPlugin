4d1ab0f25a2db02de8e45865ba81679b2073a007
StudyPi <StudyPi@mail.com>
2024-04-13 14:14:44 +0100

    oopz

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ nohup python3 GetSSIDs.py &> /dev/null
 nohup python3 ./ssid_csv_viewer.py ssid.csv 5 &> /dev/null
 nohup sudo rm -f stats.zip &> /dev/null
 nohup zip stats.zip probes.png ssid.csv &> /dev/null
-nohup bash ./stop.sh &> /dev/nullMORE STA
+nohup bash ./stop.sh &> /dev/null
 nohup kismet &> kismet.log &
 sleep 1
 nohup kismet_cap_linux_wifi --connect 127.0.0.1:2501 --source wlan0:name=Scanner --user username --password password &> Scanner.log &
