06252d325ea2610800610108270fce70783a7705
StudyPi <StudyPi@mail.com>
2024-04-17 14:45:53 +0100

    dual interface

--- start_realtime.sh
+++ start_realtime.sh
@@ -7,5 +7,6 @@ CSV_FILE="probes.csv"
 nohup ./stop_realtime.sh
 nohup touch $CSV_FILE &> /dev/null
 nohup chmod ugo+r $CSV_FILE &> /dev/null
-nohup stdbuf -i0 -o0 -e0 tshark -l -i wlan0 -Y 'wlan.fc.type==0 and wlan.fc.type_subtype==4 and wlan.ssid != ""' -T fields -e frame.time_epoch -e wlan.ta -e wlan.ra -e wlan.sa -e wlan.da -e frame.len -e wlan_radio.channel -e wlan_radio.signal_dbm -e wlan.ssid -E separator=, > $CSV_FILE &> /dev/null &
+nohup stdbuf -i0 -o0 -e0 tshark -l -i wlan0 -Y 'wlan.fc.type==0 and wlan.fc.type_subtype==4 and wlan.ssid != ""' -T fields -e frame.time_epoch -e wlan.ta -e wlan.ra -e wlan.sa -e wlan.da -e frame.len -e wlan_radio.channel -e wlan_radio.signal_dbm -e wlan.ssid -E separator=, >> $CSV_FILE &> /dev/null &
+nohup stdbuf -i0 -o0 -e0 tshark -l -i wlan1 -Y 'wlan.fc.type==0 and wlan.fc.type_subtype==4 and wlan.ssid != ""' -T fields -e frame.time_epoch -e wlan.ta -e wlan.ra -e wlan.sa -e wlan.da -e frame.len -e wlan_radio.channel -e wlan_radio.signal_dbm -e wlan.ssid -E separator=, >> $CSV_FILE &> /dev/null &
 nohup sudo -H -u pi python3 ./process_real_time_probes.py process_real_time_probes.cfg $CSV_FILE &> /dev/null &
