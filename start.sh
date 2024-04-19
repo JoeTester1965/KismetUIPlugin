f121b8628f51d7c1b311f2cc16cb68bb211d90ce
StudyPi <StudyPi@mail.com>
2024-04-19 11:46:16 +0100

    archive probes

--- start.sh
+++ start.sh
@@ -5,3 +5,5 @@ sleep 1
 nohup kismet_cap_linux_wifi --connect 127.0.0.1:2501 --source wlan0:name=Scanner --user username --password password &> Scanner.log &
 nohup kismet_cap_linux_wifi --connect 127.0.0.1:2501 --source wlan1:name=Collector,channel=1 --user username --password password &> Collector.log &
 nohup python3 KismetUIPlugin.py &>KismetUIPlugin.log &
+
+
--- stop_realtime.sh
+++ stop_realtime.sh
@@ -4,6 +4,9 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 CSV_FILE="probes.csv"
+ARCHIVE_CSV_FILE="probes_archive.csv"
+nohup cat $CSV_FILE >> $ARCHIVE_CSV_FILE &> /dev/null
+nohup rm -f $CSV_FILE &> /dev/null
 nohup rm -f $CSV_FILE &> /dev/null
 pkill -f tshark &> /dev/null
 pkill -f process_real_time_probes.py &> /dev/null
