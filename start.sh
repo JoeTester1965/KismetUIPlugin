0aa39efe11be0e998a2aead482c3854d317e1c19
StudyPi <StudyPi@mail.com>
2024-04-23 17:25:03 +0100

    doh

--- start.sh
+++ start.sh
@@ -2,8 +2,7 @@
 nohup bash ./stop.sh &> /dev/null
 nohup kismet &> kismet.log &
 sleep 1
-nohup kismet_cap_linux_wifi --connect 127.0.0.1:2501 --source wlan0:name=Scanner --user username --password password &> Scanner.log &
-nohup kismet_cap_linux_wifi --connect 127.0.0.1:2501 --source wlan1:name=Collector,channel=1 --user username --password password &> Collector.log &
+nohup kismet_cap_linux_wifi --connect 127.0.0.1:2501 --source wlan1:name=Scanner1 --user username --password password &> Scanner.log &
+nohup kismet_cap_linux_wifi --connect 127.0.0.1:2501 --source wlan2:name=Scanner2, --user username --password password &> Collector.log &
+nohup kismet_cap_linux_wifi --connect 127.0.0.1:2501 --source wlan0:name=Collector,channel=1 --user username --password password &> Collector.log &
 nohup python3 KismetUIPlugin.py &>KismetUIPlugin.log &
-
-
