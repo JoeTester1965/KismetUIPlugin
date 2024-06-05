0ad7f4f6eca54508f095c64259efff68dd28e0f6
D P <24988751+JoeTester1965@users.noreply.github.com>
2024-06-05 10:19:57 +0100

    Only archive previous days probes for graph

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 nohup bash ./stop.sh &> /dev/null
-nohup python3 probe_viewer.py ./probes_archive.csv &
 nohup kismet &> kismet.log &
 sleep 1
 #nohup kismet_cap_linux_wifi --connect 192.168.1.50:2501 --source wlan0:name=RemoteCollectorExample,channel=1 --user username --password password &> Collector.log &
--- stop_realtime.sh
+++ stop_realtime.sh
@@ -5,10 +5,9 @@ if [ "$EUID" -ne 0 ]
 fi
 CSV_FILE="probes.csv"
 ARCHIVE_CSV_FILE="probes_archive.csv"
+nohup rm -f $ARCHIVE_CSV_FILE > /dev/null
 if [ -f $CSV_FILE ]; then
-	nohup cat $CSV_FILE >> $ARCHIVE_CSV_FILE
-	nohup rm -f $CSV_FILE > /dev/null
-	nohup rm -f $CSV_FILE > /dev/null
+	nohup mv $CSV_FILE $ARCHIVE_CSV_FILE
 fi
 pkill -f tshark > /dev/null
 pkill -f process_real_time_probes.py > /dev/null
