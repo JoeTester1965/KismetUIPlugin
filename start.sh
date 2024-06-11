a1c83ac4c79ef667015097afa922ebf64ba7d010
ShedPi <ShedPi@Shed>
2024-06-11 14:39:33 +0100

    Leared about source /bin/activate !

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#set below to approprate directory if unsing a venv
-python_command="python3"
-
 nohup bash ./stop.sh &> /dev/null
 nohup kismet &> kismet.log &
 sleep 1
 nohup kismet_cap_linux_wifi --connect 192.168.1.50:2501 --source wlan0:name=RemoteCollectorExample,channel=1 --user username --password password &> Collector.log &
-nohup $python_command KismetUIPlugin.py &>KismetUIPlugin.log &
+nohup python3 KismetUIPlugin.py &>KismetUIPlugin.log &
