4054f689015863b3986bed1ad4879f81b8146bdd
ShedPi <ShedPi@Shed>
2024-06-13 10:17:12 +0100

    Added decent virtual env handling

--- start.sh
+++ start.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+
+VIRTUAL_ENV_IF_NEEDED=$PWD/bin/activate
+
+#Use a python virtual env if needed
+if [ -s $VIRTUAL_ENV_IF_NEEDED ]; then
+	source $VIRTUAL_ENV_IF_NEEDED
+else
+    echo "No local python virtual env at $VIRTUAL_ENV_IF_NEEDED is being used "
+fi
+
 CSV_FILE="probes.csv"
+ARCHIVE_CSV_FILE="probes_archive.csv"
 CONFIG_FILE="process_real_time_probes.cfg"  
 nohup bash ./stop.sh &> /dev/null
 
-nohup touch $CSV_FILE &> /dev/null
+if [ -s $CSV_FILE ]; then
+	nohup mv $CSV_FILE $ARCHIVE_CSV_FILE &> /dev/null
+else
+    nohup touch $CSV_FILE &> /dev/null
+fi
 
 #tshark must be configured for non root users, use 'sudo dpkg-reconfigure wireshark-common' if needed, monitor interface address as produced by kismet 
 nohup tshark -Q -l -i wlan1mon -Y 'wlan.fc.type==0 and wlan.fc.type_subtype==4 and wlan.ssid != ""' -T fields -e frame.time_epoch -e wlan.ta -e wlan.ra -e wlan.sa -e wlan.da -e frame.len -e wlan_radio.channel -e wlan_radio.signal_dbm -e wlan.ssid -E separator=, >> $CSV_FILE 2>/dev/null &
@@ -11,7 +26,7 @@ nohup tshark -Q -l -i wlan1mon -Y 'wlan.fc.type==0 and wlan.fc.type_subtype==4 a
 nohup python3 process_real_time_probes.py $CONFIG_FILE $CSV_FILE 2>/dev/null &
 
 #produce a pretty picture of probes found on last tun
-nohup python3 probe_viewer.py ./probes_archive.csv &> /dev/null &
+nohup python3 probe_viewer.py $ARCHIVE_CSV_FILE &> /dev/null &
 
 nohup kismet &> kismet.log & 
 nohup python3 KismetUIPlugin.py &>KismetUIPlugin.log &
--- stop.sh
+++ stop.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-CSV_FILE="probes.csv"
-ARCHIVE_CSV_FILE="probes_archive.csv"
-
 nohup pkill -f kismet_cap_linux_wifi &> /dev/null
 nohup pkill -f kismet &> /dev/null
 nohup pkill -f KismetUIPlugin.py &> /dev/null
@@ -14,7 +11,3 @@ nohup rm *.log &> /dev/null
 nohup rm edge_df.csv &> /dev/null
 nohup rm nohup.out &> /dev/null
 pkill -f tshark > /dev/null
-
-if [ -f $CSV_FILE ]; then
-	nohup mv $CSV_FILE $ARCHIVE_CSV_FILE &> /dev/null
-fi
