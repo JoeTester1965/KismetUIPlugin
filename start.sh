51341d89b13b640cb64ded0eb6f93103c219543d
joetester1965 at maildotcom <24988751+JoeTester1965@users.noreply.github.com>
2024-07-11 16:53:55 +0100

    Update start.sh - interaface must exist for tshark, example here is for wlan1 being configured in /etc/kismet.conf

--- start.sh
+++ start.sh
@@ -19,10 +19,6 @@ if [ -s $CSV_FILE ]; then
 else
     nohup touch $CSV_FILE &> /dev/null
 fi
-
-#tshark must be configured for non root users, use 'sudo dpkg-reconfigure wireshark-common' if needed, monitor interface address as produced by kismet 
-nohup tshark -Q -l -i wlan1mon -Y 'wlan.fc.type==0 and wlan.fc.type_subtype==4 and wlan.ssid != ""' -T fields -e frame.time_epoch -e wlan.ta -e wlan.ra -e wlan.sa -e wlan.da -e frame.len -e wlan_radio.channel -e wlan_radio.signal_dbm -e wlan.ssid -E separator=, >> $CSV_FILE 2>/dev/null &
-
 nohup python3 process_real_time_probes.py $CONFIG_FILE $CSV_FILE 2>/dev/null &
 
 #produce a pretty picture of probes found on last tun
@@ -30,3 +26,8 @@ nohup python3 probe_viewer.py $ARCHIVE_CSV_FILE &> /dev/null &
 
 nohup kismet &> kismet.log & 
 nohup python3 KismetUIPlugin.py &>KismetUIPlugin.log &
+
+sleep 1.0s
+
+#tshark must be configured for non root users, use 'sudo dpkg-reconfigure wireshark-common' if needed, monitor interface address as produced by kismet 
+nohup tshark -Q -l -i wlan1mon -Y 'wlan.fc.type==0 and wlan.fc.type_subtype==4 and wlan.ssid != ""' -T fields -e frame.time_epoch -e wlan.ta -e wlan.ra -e wlan.sa -e wlan.da -e frame.len -e wlan_radio.channel -e wlan_radio.signal_dbm -e wlan.ssid -E separator=, >> $CSV_FILE 2>/dev/null &
