2a91a9fd341f6379c58b3f3c04693545308de0f2
upstairs <upstairs@house.com>
2025-03-11 13:46:31 +0000

    doc

--- start.sh
+++ start.sh
@@ -26,5 +26,5 @@ nohup python3 KismetUIPlugin.py &>KismetUIPlugin.log &
 
 sleep 1.0s
 
-#tshark must be configured for non root users, use 'sudo dpkg-reconfigure wireshark-common' if needed, monitor interface address as produced by kismet 
+#tshark must be configured for non root users, use 'sudo dpkg-reconfigure wireshark-common' if needed then 'sudo usermod -aG wireshark myusername' then use monitor interface address as produced by kismet 
 nohup tshark -Q -l -i wlan0mon -Y 'wlan.fc.type==0 and wlan.fc.type_subtype==4 and wlan.ssid != ""' -T fields -e frame.time_epoch -e wlan.ta -e wlan.ra -e wlan.sa -e wlan.da -e frame.len -e wlan_radio.channel -e wlan_radio.signal_dbm -e wlan.ssid -E separator=, >> $CSV_FILE 2>/dev/null &
