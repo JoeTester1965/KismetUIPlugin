7f27844d0883fe3caaafb3e98efb2ae6bf69cdda
upstairs <upstairs@house.com>
2025-09-26 17:03:55 +0100

    s

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CSV_FILE=probes.csv
+
 VIRTUAL_ENV_IF_NEEDED=$PWD/venv/bin/activate
 
 #Use a python virtual env if needed
@@ -17,3 +19,6 @@ nohup python3 process_real_time_probes.py $CONFIG_FILE $CSV_FILE 2>/dev/null &
 
 nohup kismet &> kismet.log & 
 nohup python3 KismetUIPlugin.py &>KismetUIPlugin.log &
+
+#tshark must be configured for non root users, use 'sudo dpkg-reconfigure wireshark-common' if needed then 'sudo usermod -aG wireshark myusername' then use monitor interface address as produced by kismet 
+nohup tshark -Q -l -i wlan0mon -Y 'wlan.fc.type==0 and wlan.fc.type_subtype==4 and wlan.ssid != ""' -T fields -e frame.time_epoch -e wlan.ta -e wlan.ra -e wlan.sa -e wlan.da -e frame.len -e wlan_radio.channel -e wlan_radio.signal_dbm -e wlan.ssid -E separator=, >> $CSV_FILE 2>/dev/null &
