55c2ca0acbec9a5bce7ffb1199052592433944cd
upstairs <upstairs@house.com>
2025-10-05 10:51:52 +0100

    Do latest probes.

---
+++ probes_latest.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+VIRTUAL_ENV_DIR=$PWD/venv
+
+#Use a python virtual env if needed
+if [ -d $VIRTUAL_ENV_DIR ]; then
+        source $VIRTUAL_ENV_DIR/bin/activate
+else
+    echo "No local python virtual env at $VIRTUAL_ENV_DIR , see README.md on how to create"
+    exit
+fi
+
+if [[ -z $1 ]]
+then
+	dir=.
+else
+	dir=$1
+fi
+
+if [[ -z $2 ]]
+then
+	TIME_INTERVAL_MINTES="5"
+else
+	TIME_INTERVAL_MINTES=$2
+fi
+
+cp -f process_real_time_probes.cfg $dir
+
+sed "s|$|,0|" probes_0.csv > probes_0_temp.csv
+sed "s|$|,1|" probes_1.csv > probes_1_temp.csv
+
+cat probes_0_temp.csv probes_1_temp.csv > probes.csv
+
+nohup python3 probes_latest.py probes.csv $TIME_INTERVAL_MINTES > probe_viewer.log 2>&1
+
+cp -f probes_latest.csv $dir/$(date +%d-%m-%Y-%H-%M)-probes_latest.csv
+
+exit
