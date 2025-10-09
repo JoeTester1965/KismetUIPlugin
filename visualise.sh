2db5a2301cb50e6faf57a904cca5334086f3f18b
upstairs <upstairs@house.com>
2025-10-09 15:30:45 +0100

    funny formatz :

--- visualise.sh
+++ visualise.sh
@@ -34,10 +34,10 @@ cat probes_0_temp.csv probes_1_temp.csv > probes.csv
 nohup python3 probe_viewer.py probes.csv $TIME_INTERVAL > probe_viewer.log 2>&1
 
 mkdir -p $dir/$(date +%d-%m-%Y)
-mkdir -p $dir/$(date +%d-%m-%Y)/$(date +%H:%M)
+mkdir -p $dir/$(date +%d-%m-%Y)/$(date +%H-%M)
 
-cp -f probes.jpg $dir/$(date +%d-%m-%Y)/$(date +%H:%M)
-cp -f probes-raw.jpg $dir/$(date +%d-%m-%Y)/$(date +%H:%M)
-cp -f probes_sorted.csv $dir/$(date +%d-%m-%Y)/$(date +%H:%M)
+cp -f probes.jpg $dir/$(date +%d-%m-%Y)/$(date +%H-%M)
+cp -f probes-raw.jpg $dir/$(date +%d-%m-%Y)/$(date +%H-%M)
+cp -f probes_sorted.csv $dir/$(date +%d-%m-%Y)/$(date +%H-%M)
 
 exit
