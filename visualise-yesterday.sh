ee91f3a66e3c0cb73801c318e32c69e05f483c0d
upstairs <upstairs@house.com>
2025-10-14 12:29:01 +0100

    Visualise all probes from day before

---
+++ visualise-yesterday.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+VIRTUAL_ENV_DIR=$PWD/venv
+
+#Use a python virtual env if needed
+if [ -d $VIRTUAL_ENV_DIR ]; then
+        source $VIRTUAL_ENV_DIR/bin/activate
+else
+    echo "No local python virtual env at $VIRTUAL_ENV_DIR , see README.md on how to create"
+    exit
+fi
+
+if [[ -z $1 ]]
+then
+	dir=.
+else
+	dir=$1
+fi
+
+TIME_INTERVAL="24h"
+
+directory=$dir/$(date -d "yesterday" +%d-%m-%Y)
+
+file=$directory/probes_sorted.csv
+
+echo $file
+
+echo $directory
+
+nohup python3 probe_viewer_sorted.py $file > probe_viewer.log 2>&1
+
+cp -f probes.jpg $directory
+cp -f probes-raw.jpg $directory
+
+exit
