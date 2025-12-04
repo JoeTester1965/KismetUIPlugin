3fdfe86d1290748ba9cf9eb7171776825b6401ac
upstairs <upstairs@house.com>
2025-12-04 11:21:06 +0000

    tidied

--- restart_devices.sh
+++ restart_devices.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 sudo airmon-ng stop wlan0mon
 sudo airmon-ng stop wlan1mon
-sudo ifconfig wlan0 down
-sudo ifconfig wlan1 down
 sudo usbreset 001/003
+sleep 1
 sudo usbreset 001/004
-sudo ifconfig wlan0 up
-sudo ifconfig wlan1 up
+sleep 1
 sudo airmon-ng check kill
 sudo airmon-ng start wlan0
 sudo airmon-ng start wlan1
