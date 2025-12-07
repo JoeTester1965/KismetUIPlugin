4d66743c0e84ecd311ec0e28b6f236f88db63ed4
upstairs <upstairs@house.com>
2025-12-07 10:05:09 +0000

    1 card

--- restart_devices.sh
+++ restart_devices.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 sudo airmon-ng stop wlan0mon
-sudo airmon-ng stop wlan1mon
-sudo usbreset 001/003
-sleep 1
-sudo usbreset 001/004
-sleep 1
+#sudo usbreset 001/004
 sudo airmon-ng check kill
 sudo airmon-ng start wlan0
-sudo airmon-ng start wlan1
 
